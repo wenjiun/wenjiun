9f226ecd565c57ea4347f8243f1f7615191e4c6f
Yap Wen Jiun <wenjiun@yahoo.com>
2023-02-09 11:46:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ class App{
 
         this.cube = new THREE.Mesh(
             new THREE.BoxBufferGeometry(1, 1, 1),
-            new THREE.MeshLambertMaterial({color:'red'})
+            new THREE.MeshLambertMaterial({color:'green'})
         );
         this.cube.position.set(0, 1.6, 4);
         this.scene.add(this.cube);
